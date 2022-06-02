b4755ae90d14c691e98e8da814ea09260727df6c
ThomasKittlaus <thomaskittlaus@gmail.com>
2022-06-02 22:38:39 +0200

    letterFinder added with post endpoint

--- backend/src/main/java/de/neuefische/backend/hangman/HangmanController.java
+++ backend/src/main/java/de/neuefische/backend/hangman/HangmanController.java
@@ -1,18 +1,35 @@
 package de.neuefische.backend.hangman;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/hangmen")
 public class HangmanController {
 
+    private final String SOLUTION = "HalloWelt".toUpperCase(Locale.ROOT);
+    private Set<String> guessedLetters = ConcurrentHashMap.newKeySet();
+
 
     @GetMapping
     public String sayHello(){
+        return hideUnknownLetters();
+    }
 
-        return "_________";
+    @PostMapping("/{letter}")
+    public String checkForLetter(@PathVariable String letter){
+        guessedLetters.add(letter.toUpperCase(Locale.ROOT));
+        return hideUnknownLetters();
+    }
+
+    private String hideUnknownLetters() {
+        return Stream.of(SOLUTION.split("(?!^)"))
+                .map(l -> guessedLetters.contains(l) ? l : "_")
+                .collect(Collectors.joining());
     }
 
 }
--- backend/src/test/java/de/neuefische/backend/HangmanIT.java
+++ backend/src/test/java/de/neuefische/backend/HangmanIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -27,9 +28,18 @@ public class HangmanIT {
     }
 
     @Test
+    @DirtiesContext
     void shouldFindLetterAInSolution() throws Exception {
         mockMvc.perform(post("/api/hangmen/a"))
                 .andExpect(content().string("_A_______"));
+    }
+
+    @Test
+    @DirtiesContext
+    void shouldFindLetterAAndLInSolution() throws Exception {
+        mockMvc.perform(post("/api/hangmen/a"));
+        mockMvc.perform(post("/api/hangmen/l"))
+                .andExpect(content().string("_ALL___L_"));
     }
 
 
